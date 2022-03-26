963ca46145977026c52a4d02f0858fbc92eabc7d
Eigenaar <Eigenaar@LAPTOP-NAFMD4PS.kpn>
2022-03-26 07:23:14 +0100

    hi team,
    it is my first commit

---
+++ src/test/java/myPackage/Test1.java
@@ -0,0 +1,18 @@
+package myPackage;
+
+public class Test1 {
+
+	public static void main(String[] args) 
+	{
+	 int a=10;
+	 int b=20;
+	 
+		int c;
+		c=a+b;
+		System.out.println(c);
+	 
+	 
+
+	}
+
+}
